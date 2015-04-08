b9a42750b9542e07d04b2a2f39baa5188ba08bec
Elliot Mitchum <elliot.mitchum@digitalgarden.com.au>
2015-04-08 14:09:53 +1000

    Whoops

--- src/equalis.jquery.js
+++ src/equalis.jquery.js
@@ -1,142 +1,83 @@
-;(function ( $, window, document, undefined ) {
+(function ( $, window, document, undefined ) {
 
-  var pluginName = "responsiveMenu",
-    defaults = {
-      elementClass: 'responsive-menu',
-      elementActiveClass: 'responsive-menu-active',
-      elementInactiveClass: 'reponsive-menu-inactive',
-      elementOpenClass: 'responsive-menu-open',
-      elementClosedClass: 'responsive-menu-closed',
-      toggleClass: 'responsive-menu-toggle',
-      toggleElement: 'button',
-      toggleText: 'Toggle Menu',
-      toggleActiveClass: 'responsive-menu-toggle-active',
-      toggleInactiveClass: 'responsive-menu-toggle-inactive',
-      toggleOpenClass: 'responsive-menu-toggle-open',
-      toggleClosedClass: 'responsive-menu-toggle-closed',
-      breakpoint: 1024
-    };
+    var pluginName = "equalis",
+        defaults = {
+            items: '> div',
+            breakpoint: 768,
+            className: pluginName
+        };
 
-  function Plugin ( element, options ) {
-    this.element = element;
-    this.$el = $(element);
-    this.settings = $.extend( {}, defaults, options );
-    this._defaults = defaults;
-    this._name = pluginName;
-    this.$toggle;
-    this.height;
-    this.init();
-  }
-
-  $.extend(Plugin.prototype, {
-    init: function () {
-      var plugin = this,
-          settings = plugin.settings,
-          width = $(window).width();
-      plugin.$toggle = plugin.createToggle();
-      plugin.$el.addClass(settings.elementClass);
-      plugin.$el.before(plugin.$toggle);
-      plugin.create();
-      $(window).resize(function(){
-        if($(this).width() != width){
-          width = $(this).width();
-          plugin.create();
-        }
-      });
-    },
-    create: function() {
-      var plugin = this,
-        settings = plugin.settings,
-        windowWidth = $(window).outerWidth();
-      if(windowWidth < settings.breakpoint) {
-        plugin.getHeight();
-        plugin.activate();
-      } else {
-        plugin.disable();
-      }
-    },
-    toggleMenu: function() {
-      var plugin = this,
-        settings = plugin.settings;
-      if(plugin.$el.hasClass(settings.elementOpenClass)) {
-        plugin.menuClose();
-      } else {
-        plugin.menuOpen();
-      }
-    },
-    menuClose: function() {
-      var plugin = this,
-        settings = plugin.settings;
-      plugin.$el.removeClass(settings.elementOpenClass);
-      plugin.$el.addClass(settings.elementClosedClass);
-      plugin.$toggle.removeClass(settings.toggleOpenClass);
-      plugin.$toggle.addClass(settings.toggleClosedClass);
-      plugin.$el.css({
-        height: 0,
-        overflow: 'hidden'
-      });
-    },
-    menuOpen: function() {
-      var plugin = this,
-        settings = plugin.settings;
-      plugin.$el.removeClass(settings.elementClosedClass);
-      plugin.$el.addClass(settings.elementOpenClass);
-      plugin.$toggle.removeClass(settings.toggleClosedClass);
-      plugin.$toggle.addClass(settings.toggleOpenClass);
-      plugin.$el.css('height', plugin.height);
-    },
-    createToggle: function() {
-      var plugin = this,
-        settings = plugin.settings,
-        toggle;
-      toggle = $('<' + settings.toggleElement + '/>');
-      toggle.text(settings.toggleText);
-      toggle.addClass(settings.toggleClass);
-      toggle.click(function(){
-        plugin.toggleMenu();
-      });
-      return toggle;
-    },
-    disable: function() {
-      var plugin = this,
-        settings = plugin.settings;
-      plugin.$el.removeClass(settings.elementActiveClass);
-      plugin.$el.addClass(settings.elementInactiveClass);
-      plugin.$el.removeClass(settings.elementOpenClass);
-      plugin.$el.removeClass(settings.elementClosedClass);
-      plugin.$toggle.removeClass(settings.toggleActiveClass);
-      plugin.$toggle.addClass(settings.toggleInactiveClass);
-      plugin.$toggle.removeClass(settings.toggleOpenClass);
-      plugin.$toggle.removeClass(settings.toggleClosedClass);
-      plugin.$toggle.hide();
-      plugin.$el.removeAttr('style');
-    },
-    activate: function() {
-      var plugin = this,
-        settings = plugin.settings;
-        windowWidth = $(window).outerWidth();
-      plugin.$el.removeClass(settings.elementInactiveClass);
-      plugin.$el.addClass(settings.elementActiveClass);
-      plugin.$toggle.removeClass(settings.toggleInactiveClass);
-      plugin.$toggle.addClass(settings.toggleActiveClass);
-      plugin.$toggle.show();
-      plugin.menuClose();
-    },
-    getHeight: function() {
-      var plugin = this;
-      plugin.$el.removeAttr('style').css('visibility', 'hidden');
-      plugin.height = plugin.$el.height();
-      plugin.$el.removeAttr('style').css('height', 0);
+    function Plugin ( element, options ) {
+        this.element = element;
+        this.settings = $.extend( {}, defaults, options );
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.elems = [];
+        this.init();
     }
-  });
 
-  $.fn[ pluginName ] = function ( options ) {
-    this.each(function() {
-      if ( !$.data( this, "plugin_" + pluginName ) ) {
-        $.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
-      }
+    $.extend(Plugin.prototype, {
+        init: function () {
+            var plugin = this;
+            $(plugin.element).addClass(plugin.settings.className);
+            $.each(plugin.settings.items, function(key, value){
+                plugin.elems.push($(plugin.element).find(value));
+            });
+            plugin.responsive();
+            $(window).resize(function(){
+                plugin.responsive();
+            });
+        },
+        responsive: function() {
+            var plugin = this,
+                breakpoint = plugin.settings.breakpoint,
+                windowWidth = $(window).width(),
+                scrollbar = window.innerWidth - windowWidth;
+            if( breakpoint && windowWidth + scrollbar >= breakpoint ) {
+                plugin.create();
+            } else {
+                plugin.destroy();
+            }
+        },
+        create: function() {
+            var plugin = this,
+                elems = plugin.elems,
+                heights = [],
+                max;
+            $.each(elems, function(key, value){
+                plugin.destroy();
+                $(value).each(function(){
+                    heights.push( $(this).outerHeight() );
+                });
+            });
+            max = Math.max.apply( Math, heights );
+            $.each(elems, function(key, value){
+                $(value).css({
+                    minHeight: max
+                });
+                if( ! $(value).hasClass('equalise-item') ) {
+                    $(this).addClass('equalise-item');
+                }
+            });
+        },
+        destroy: function() {
+            var plugin = this;
+            $.each(this.elems, function(key, value){
+                $(value).each(function(){
+                    $(this).removeClass('equalise-item');
+                    $(this).removeAttr('style');
+                });
+            });
+        }
     });
-    return this;
-  };
+
+    $.fn[ pluginName ] = function ( options ) {
+        this.each(function() {
+            if ( !$.data( this, "plugin_" + pluginName ) ) {
+                $.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+            }
+        });
+        return this;
+    };
 
 })( jQuery, window, document );
